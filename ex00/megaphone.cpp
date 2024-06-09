4cddb60f0482074b7c3eec8f137f0129fc530637
smizuoch <shotaro.mizuochi@proton.me>
2024-06-09 17:40:30 +0900

    add

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,11 +1,20 @@
-#include <iostream>
 #include <cctype>
+#include <iostream>
 
 int main(int ac, char **av)
-{if (ac == 1){std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;return 1;}for(int i = 1; i < ac; i++){for (int j = 0; av[i][j] != '\0'; j++)std::cout << static_cast<char>(std::toupper(av[i][j]));
-		if (i < ac - 1)
-			std::cout << " ";
-	}
-	std::cout << std::endl;
-	return 0;
+{
+    if (ac == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 1;
+    }
+    for (int i = 1; i < ac; i++)
+    {
+        for (int j = 0; av[i][j] != '\0'; j++)
+            std::cout << static_cast<char>(std::toupper(av[i][j]));
+        if (i < ac - 1)
+            std::cout << " ";
+    }
+    std::cout << std::endl;
+    return 0;
 }
