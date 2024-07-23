48fcc9eb067842c3e75aff60ee83387750e40ea7
smizuoch <shotaro.mizuochi@proton.me>
2024-07-23 12:51:25 +0900

    fix ctrl-d

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,46 +6,37 @@ Contact inputContact() {
     std::string first, last, nick, phone, secret;
 
     std::cout << "Enter first name: ";
-	if (std::cin.fail()) {
-		std::cout << "Invalid input. Please try again." << std::endl;
-		exit(1);
-	}
-	if (std::cin.eof()) {
-		std::cout << "Invalid input. Please try again." << std::endl;
-		exit(1);
-	}
     std::cin >> first;
+	if (std::cin.fail()) {
+		std::cout << "Invalid input. Please try again." << std::endl;
+		exit(1);
+	}
+	if (std::cin.eof()) {
+		std::cout << "Invalid input. Please try again." << std::endl;
+		exit(1);
+	}
     std::cout << "Enter last name: ";
-	if (std::cin.fail()) {
-		std::cout << "Invalid input. Please try again." << std::endl;
-		exit(1);
-	}
-	if (std::cin.eof()) {
-		std::cout << "Invalid input. Please try again." << std::endl;
-		exit(1);
-	}
     std::cin >> last;
+	if (std::cin.fail()) {
+		std::cout << "Invalid input. Please try again." << std::endl;
+		exit(1);
+	}
+	if (std::cin.eof()) {
+		std::cout << "Invalid input. Please try again." << std::endl;
+		exit(1);
+	}
     std::cout << "Enter nickname: ";
-	if (std::cin.fail()) {
-		std::cout << "Invalid input. Please try again." << std::endl;
-		exit(1);
-	}
-	if (std::cin.eof()) {
-		std::cout << "Invalid input. Please try again." << std::endl;
-		exit(1);
-	}
     std::cin >> nick;
+	if (std::cin.fail()) {
+		std::cout << "Invalid input. Please try again." << std::endl;
+		exit(1);
+	}
+	if (std::cin.eof()) {
+		std::cout << "Invalid input. Please try again." << std::endl;
+		exit(1);
+	}
     std::cout << "Enter phone number: ";
-	if (std::cin.fail()) {
-		std::cout << "Invalid input. Please try again." << std::endl;
-		exit(1);
-	}
-	if (std::cin.eof()) {
-		std::cout << "Invalid input. Please try again." << std::endl;
-		exit(1);
-	}
     std::cin >> phone;
-    std::cout << "Enter darkest secret: ";
 	if (std::cin.fail()) {
 		std::cout << "Invalid input. Please try again." << std::endl;
 		exit(1);
@@ -54,7 +45,16 @@ Contact inputContact() {
 		std::cout << "Invalid input. Please try again." << std::endl;
 		exit(1);
 	}
+    std::cout << "Enter darkest secret: ";
     std::cin >> secret;
+	if (std::cin.fail()) {
+		std::cout << "Invalid input. Please try again." << std::endl;
+		exit(1);
+	}
+	if (std::cin.eof()) {
+		std::cout << "Invalid input. Please try again." << std::endl;
+		exit(1);
+	}
 
     return Contact(first, last, nick, phone, secret);
 }
